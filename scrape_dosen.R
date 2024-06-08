c80ac0bd54f2633525553a7cfa541437b1fa95fb
Anwar Fajar Rizki <158245430+AnFaRiz@users.noreply.github.com>
2024-06-08 13:13:14 +0700

    Update scrape_dosen.R

--- scrape_dosen.R
+++ scrape_dosen.R
@@ -22,7 +22,6 @@ for (letter in letters) {
     df_master <- rbind(df_master, a2)
   }
 }
-df_master <- filter(df_master, rowSums(df_master == "No results found.") != ncol(df_master))
 dosen <- df_master[,-1]
 dosen$Waktu_Scraping<-Sys.Date()
 
