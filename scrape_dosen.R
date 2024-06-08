5097d271f56eeb03081c6845a62c6532282f6693
Anwar Fajar Rizki <158245430+AnFaRiz@users.noreply.github.com>
2024-06-08 12:36:38 +0700

    Update scrape_dosen.R

--- scrape_dosen.R
+++ scrape_dosen.R
@@ -23,6 +23,8 @@ for (letter in letters) {
 }
 dosen <- df_master[,-1]
 dosen$Waktu_Scraping<-Sys.Date()
+dosen <- dosen %>%
+  filter(rowSums(. == "No results found.") != ncol(.))
 
 #MONGODB
 message('Input Data to MongoDB Atlas')
